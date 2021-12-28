519b94042831cce08cc88efe462002566eec27a2
Pacio <4995125@163.com>
2021-12-29 01:57:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git OpenWrt-Packages https://github.com/Boos4721/OpenWrt-Packages' >>feeds.conf.default
+
