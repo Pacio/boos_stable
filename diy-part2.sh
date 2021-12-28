1a1a313be18333c51b6b5e9f4c1ee24d47eb022a
Pacio <4995125@163.com>
2021-12-29 01:57:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-#rm -f target/linux/ipq807x/patches-5.10/700-ipq8074-overclock-cpu-2.2ghz.patch
-
 # Modify default IP
 sed -i 's/10.10.10.1/192.168.2.1/g' package/base-files/files/bin/config_generate
